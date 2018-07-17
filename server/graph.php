7ea5fae588e4ab1f0a9dc9fcdf4d614ffda70202
vrodas <prjchec.vrodas@umanizales.edu.co>
2018-07-17 15:21:04 -0500

    cambios nuevos en el sistema de monitoreo 1

--- server/graph.php
+++ server/graph.php
@@ -1,1 +1,9 @@
-require
+<?php
+require 'getBusqueda.php';
+require 'getResultado.php';
+require 'getCriterioBusqueda.php';
+require 'getUsoWeb.php';
+require 'getIngresoPorHora.php';
+require 'getIngresoPorDia.php';
+
+$api = new ChatbotApi();
